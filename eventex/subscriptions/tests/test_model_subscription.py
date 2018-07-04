43497b0f52a0cb34362f140ff2b6401f3c36a08e
okadaelaine <3l41n3@gmail.com>
2018-07-04 23:50:26 +0900

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Elaine Okada',
+            cpf='12345678901',
+            email='3l41n3@gmail.com',
+            phone='11-12345-1234'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto create attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
