9356d4c2fa37e605a087b54fd279bc8f1783eed9
okadaelaine <3l41n3@gmail.com>
2018-08-12 17:11:58 +0900

    Email ou telefone opcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -12,18 +12,33 @@ class SubscriptionFormTest(TestCase):
 
     def test_cpf_is_digit(self):
         """CPF must only accept digits."""
-        form = self.make_validate_form(cpf='ABCD5678901')
+        form = self.make_validated_form(cpf='ABCD5678901')
         self.assertFormErrorCode(form, 'cpf', 'digits')
 
     def test_cpf_has_11_digits(self):
         """CPF must have 11 digits"""
-        form = self.make_validate_form(cpf='1234')
+        form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
     def test_name_must_be_capitalized(self):
         """Name must be capitalized"""
-        form = self.make_validate_form(name='ELAINE okada')
+        form = self.make_validated_form(name='ELAINE okada')
         self.assertEqual('Elaine Okada', form.cleaned_data['name'])
+
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Telephone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and phone are optional, but at least one must be informed"""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
@@ -36,7 +51,7 @@ class SubscriptionFormTest(TestCase):
         errors_list = errors[field]
         self.assertListEqual([msg], errors_list)
 
-    def make_validate_form(self, **kwargs):
+    def make_validated_form(self, **kwargs):
         valid = dict(name='Elaine Okada', cpf='12345678901',
                     email='3l41n3@gmail.com', phone='11-12345-1234')
 
